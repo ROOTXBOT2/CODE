a0ce479ed77b782c4d2d298280b5900de6495d6e
ROOTXBOT2 <155366776+ROOTXBOT2@users.noreply.github.com>
2024-01-01 21:56:34 +0900

    Update 특수문자 출력하기.py
    풀이 추가

--- 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.py
+++ 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.py
@@ -1,3 +1,6 @@
+# print(r'!@#$%^&*(\'"<>?:;')
+# raw 를 의미하는 r로, regex 에서 pattern 설정 시 escape 문자를 많이 써야해서 자주 사용합니다
+
 # 특수 문자들을 문자열로 정의
 special_chars = """!@#$%^&*(\\'"<>?:;"""
 print(special_chars)
