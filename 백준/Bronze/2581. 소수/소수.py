ad3eb196fbd311c77a85d5736759bb68af403f47
ROOTXBOT2 <155366776+ROOTXBOT2@users.noreply.github.com>
2024-01-04 09:49:44 +0900

    [Bronze II] Title: 소수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.py
+++ 백준/Bronze/2581. 소수/소수.py
@@ -1,18 +1,21 @@
 start_num = int(input())
 last_num = int(input())
 
-sosu_list = []
-for num in range(start_num, last_num+1):
-    error = 0
-    if num > 1 :
-        for i in range(2, num):  # 2부터 num-1까지
-            if num % i == 0:
-                error += 1
-                break  # 2부터 num-1까지 나눈 몫이 0이면 error가 증가하고 for문을 끝냄
-        if error == 0:
-            sosu_list.append(num)  # error가 없으면 소수리스트에 추가
-            
-if len(sosu_list) > 0 :
+# 에라토스테네스의 체 초기화
+sieve = [True] * (last_num + 1)
+sieve[0:2] = [False, False]  # 0과 1은 소수가 아님
+
+# 에라토스테네스의 체 알고리즘
+for i in range(2, int(last_num**0.5) + 1):
+    if sieve[i]:
+        for j in range(i*2, last_num + 1, i):
+            sieve[j] = False
+
+# 소수 리스트 생성
+sosu_list = [i for i in range(start_num, last_num + 1) if sieve[i]]
+
+# 결과 출력
+if sosu_list:
     print(sum(sosu_list))
     print(min(sosu_list))
 else:
