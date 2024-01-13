d195a51a6b26260f93a3f16f7fd8a4fa1312dbe4
ROOTXBOT2 <155366776+ROOTXBOT2@users.noreply.github.com>
2024-01-13 22:05:06 +0900

    [level Lv.0] Title: 수 조작하기 1, Time: 3.80 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+def solution(n, control):
+    answer = 0
+    for i in control:
+        if i == "w":
+            n += 1
+        elif i == "s":
+            n -= 1
+        elif i == "d":
+            n += 10
+        elif i == "a":
+            n -= 10
+        else:
+            return "Wrong string in control"
+    return n
