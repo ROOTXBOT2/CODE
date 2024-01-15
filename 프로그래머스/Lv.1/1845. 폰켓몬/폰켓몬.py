aef83d1cf97628bb07b895962b9edcb5750e9a0e
ROOTXBOT2 <155366776+ROOTXBOT2@users.noreply.github.com>
2024-01-15 10:23:36 +0900

    [level Lv.1] Title: 폰켓몬, Time: 0.25 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/Lv.1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/Lv.1/1845. 폰켓몬/폰켓몬.py
@@ -1,9 +1,2 @@
-from collections import Counter
 def solution(nums):
-    answer = 0
-    
-    if len(nums)/2 >= len(Counter(nums)):
-        answer = len(Counter(nums))
-    elif len(nums)/2 < len(Counter(nums)):
-        answer = len(nums)/2
-    return answer
+    return min(len(nums)/2, len(set(nums)))
