c454ece63696e374aba27870d033e4d75ca4a55b
ROOTXBOT2 <155366776+ROOTXBOT2@users.noreply.github.com>
2024-01-16 23:47:50 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.02 ms, Memory: 9.93 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -1,4 +1,6 @@
 def solution(my_string, is_suffix):
     suffixs = [my_string[-i:] for i in range(len(my_string))]
     answer = 1 if is_suffix in suffixs else 0
+    # if my_string[-len(is_suffix):]==is_suffix: return 1 
+    # return 0
     return answer
