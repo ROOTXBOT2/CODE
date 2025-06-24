9b664c2824e11cc539fdaf4903c46c642fca53db
Hwang DoHyeun <155366776+ROOTXBOT2@users.noreply.github.com>
2025-06-24 14:02:14 +0900

    [level 2] Title: 올바른 괄호, Time: 18.03 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,12 +1,21 @@
+import java.util.Stack;
+
 class Solution {
     boolean solution(String s) {
-        int sum = 0;
-        for (char c : s.toCharArray()) {
-            if (c == '(') sum++;
-            else sum--;
+        Stack<Character> stack = new Stack<>();
 
-            if (sum < 0) return false;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c); // 여는 괄호는 무조건 push
+            } else {
+                if (stack.isEmpty()) {
+                    return false; // 닫는 괄호인데 매칭될 여는 괄호 없음
+                }
+                stack.pop(); // 매칭되는 여는 괄호 pop
+            }
         }
-        return sum == 0;
+
+        // 끝까지 처리 후 스택이 비어 있어야 정상
+        return stack.isEmpty();
     }
 }
