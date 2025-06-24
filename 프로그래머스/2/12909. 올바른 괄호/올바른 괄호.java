3220d438dc490224489bbe1ccdfaf54a3c095624
Hwang DoHyeun <155366776+ROOTXBOT2@users.noreply.github.com>
2025-06-24 14:04:15 +0900

    Update 올바른 괄호.java

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -19,3 +19,15 @@ class Solution {
         return stack.isEmpty();
     }
 }
+// class Solution {
+//     boolean solution(String s) {
+//         int sum = 0;
+//         for (char c : s.toCharArray()) {
+//             if (c == '(') sum++;
+//             else sum--;
+
+//             if (sum < 0) return false;
+//         }
+//         return sum == 0;
+//     }
+// }
