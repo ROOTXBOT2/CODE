13371136e4fbb5521275c5043390155f49f3417e
Hwang DoHyeun <155366776+ROOTXBOT2@users.noreply.github.com>
2025-07-01 11:17:03 +0900

    [level 2] Title: H-Index, Time: 3.01 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,17 +1,6 @@
-import java.util.Arrays;
-
 class Solution {
     public int solution(int[] citations) {
-        Arrays.sort(citations); // 오름차순 정렬
-
-        int n = citations.length;
-        for (int h = n; h >= 0; h--) {
-            int count = 0;
-            for (int c : citations) {
-                if (c >= h) count++;
-            }
-            if (count >= h) return h;
-        }
-        return 0;
+        int answer = 0;
+        return answer;
     }
 }
