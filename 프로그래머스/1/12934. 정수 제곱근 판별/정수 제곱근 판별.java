6ac63a3ce4c281d5bf5c324a2e66019968b851c4
Hwang DoHyeun <155366776+ROOTXBOT2@users.noreply.github.com>
2025-07-07 09:31:36 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.06 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,24 @@
+class Solution {
+    public long solution(long n) {
+        long left = 1;
+        long right = n;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+
+            if (mid > n / mid) {
+                right = mid - 1;
+            } else if (mid < n / mid) {
+                left = mid + 1;
+            } else {
+                if (mid * mid == n) {  // 안전하게 한 번만 곱
+                    return (mid + 1) * (mid + 1);
+                } else {
+                    return -1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
